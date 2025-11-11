8b839b6fd989e07a4e55b1ae15f43406c742d57a
Meghan Hoffman <meghanhoffman@Meghans-MacBook-Pro-3.local>
2025-11-11 16:56:04 -0500

    Binning the KNN

--- modules/knn.R
+++ modules/knn.R
@@ -25,3 +25,21 @@ knn_m1 <- knn(train = train[,-ncol(train)],
               k = 15, prob = T)
 
 saveRDS(knn_m1, "models/KNNModelM1.RDS")
+
+knn_m1 <- readRDS("models/KNNModelM1.RDS")
+
+#Pull out the actual probabilities for the KNN model
+
+knn_p1 <- attributes(knn_m1)$prob
+summary(knn_p1)
+
+# prob is the proportion of votes to the winning class
+# The predicted value is the proportion of 1s that were near the data point. If the predicted value is 1, then the probability is prob. If the predicted value is 0, then the probability is (1-prob)
+knn_p1 <- ifelse(knn_m1 == 1, attributes(knn_m1)$prob, (1-attributes(knn_m1)$prob))
+summary(knn_p1)
+
+
+#Binning the predictions
+knn_pred1 <- ifelse(knn_p1 >= 0.4, 1, 0)
+
+
